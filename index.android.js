258dfc58301a4d90404be89d0b46aef60a9f84d7
satvik <satvik.jagannath7@gmail.com>
2015-12-28 16:18:11 +0530

    Code Cleanup

--- index.android.js
+++ index.android.js
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 var React = require('react-native');
 var {AppRegistry} = React;
 
 var reactnativeAlphabeticalListView = require('./components/SelectableSectionsListView');
-
-module.exports = reactnativeAlphabeticalListView;
 
 AppRegistry.registerComponent('reactnativeAlphabeticalListView', () => reactnativeAlphabeticalListView);
